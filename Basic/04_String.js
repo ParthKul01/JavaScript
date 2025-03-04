94bbacbc956b36e81227373a8f8f73f17e15c2f4
Parth Kulkarni <137262968+ParthKul01@users.noreply.github.com>
2025-03-04 02:46:38 +0000

    String Conversion

---
+++ Basic/04_String.js
@@ -0,0 +1,14 @@
+/*let value  = 3 ;
+let negvalue = -value ;
+console.log(negvalue) ; */
+
+let str1 = "Parth" 
+let str2 = " Kulkarni" 
+
+console.log(str1 + str2) ; 
+
+console.log("1" + (2+2)) ;
+
+console.log((10+22) + "5") ;
+
+console.log(2 === 2) ; // Strict check it dont only check the value but also check the dataType
