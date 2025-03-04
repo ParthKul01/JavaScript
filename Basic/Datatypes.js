cae4fa06883dc231d1787b21488840f35d9a9c0c
Parth Kulkarni <137262968+ParthKul01@users.noreply.github.com>
2025-03-04 14:13:51 +0000

    Datatypes Summary

--- Basic/Datatypes.js
+++ Basic/Datatypes.js
@@ -9,3 +9,40 @@ let arr = null ; //null means Empty
 console.log(typeof("parth"))
 
 console.log(typeof(null)) ;
+
+// Primitive 
+// 7 types : String ,  Number , Boolean , null , undefined , symbol , BigInt
+
+// Javascript is a Dyanamic Language means it identifies the data type based on user input 
+
+// Reference (Non primitive)
+
+//Array , Objects , Functions 
+
+const score = 100 ;
+const scoreValue  = 10.11  ;
+
+let id = Symbol('123') ;
+let id2 = Symbol('123') ;
+console.log("Comparison");
+console.log(id === id2);
+
+
+const bigNumber = BigInt(1232435456534253453) ;
+console.log(typeof bigNumber) ;
+
+
+let parth = {
+    name : "Parht" ,
+    age : 18 ,
+    address : "Loni"
+}
+
+let fun = function(){
+    console.log("Hey this is my first function ") ;
+}
+
+console.log(parth) ;
+console.log(typeof fun) ;
+
+console.log(typeof id) ;
